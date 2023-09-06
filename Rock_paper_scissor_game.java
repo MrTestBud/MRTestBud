d10b4d6d26f1176abd1a6d1ace2850da783c3e84
MrTestBud <131723970+MrTestBud@users.noreply.github.com>
2023-09-07 03:23:05 +0530

    Update Rock_paper_scissor_game.java
    Made it so only have to run it once

--- Rock_paper_scissor_game.java
+++ Rock_paper_scissor_game.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 
 public class Rock_paper_Sccisor_game_ {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please Choose \n 0.Rock \n 1.Paper \n 2.Scissor");
-        int a = scan.nextInt();
-        switch (a){
-            case 0 -> System.out.println("You Have Chosen Rock");
-            case 1 -> System.out.println("You Have Chosen Paper");
-            case 2 -> System.out.println("You Have Chosen Scissor");
-        }
-        Random r = new Random();
-        int c = r.nextInt(3);
-        switch (c) {
-            case 0 -> System.out.println("Computer Have Chosen Rock");
-            case 1 -> System.out.println("Computer Have Chosen Paper");
-            case 2 -> System.out.println("Computer Have Chosen Scissor");
-        }
-        if (a==0 && c==1 || a==1 && c==2 || a==2 && c==0){
-            System.out.println("You lost, Try Again !!");
-        }else if (c==0 && a==1 || c==1 && a==2 || c==2 && a==0){
-            System.out.println("You Won, Play Again ? ");
-        } else if (a==c) {
-            System.out.println("You Tied, Try Again !!");
-
-        }
-
-
+       while (true){     
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Please Choose \n 0.Rock \n 1.Paper \n 2.Scissor");
+            int a = scan.nextInt();
+            switch (a){
+                case 0 -> System.out.println("You Have Chosen Rock");
+                case 1 -> System.out.println("You Have Chosen Paper");
+                case 2 -> System.out.println("You Have Chosen Scissor");
+            }
+            Random r = new Random();
+            int c = r.nextInt(3);
+            switch (c) {
+                case 0 -> System.out.println("Computer Have Chosen Rock");
+                case 1 -> System.out.println("Computer Have Chosen Paper");
+                case 2 -> System.out.println("Computer Have Chosen Scissor");
+            }
+            if (a==0 && c==1 || a==1 && c==2 || a==2 && c==0){
+                System.out.println("You lost, Try Again !!");
+            }else if (c==0 && a==1 || c==1 && a==2 || c==2 && a==0){
+                System.out.println("You Won, Play Again ? ");
+            } else if (a==c) {
+                System.out.println("You Tied, Try Again !!");
+    
+            }
+       }
     }
 }
